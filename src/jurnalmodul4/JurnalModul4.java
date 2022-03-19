1bf808e1705ef31aa70ad782a9ff8954e949367a
Dedygans <dedyharahapchannel@gmail.com>
2022-03-19 14:33:50 +0800

    first commit

---
+++ src/jurnalmodul4/JurnalModul4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnalmodul4;
+
+/**
+ *
+ * @author dedyh
+ */
+public class JurnalModul4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
